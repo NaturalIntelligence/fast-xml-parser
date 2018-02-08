f5d348ced6afcc794acfb54f6ea0d6c04f0752c8
Amit Gupta <amitgupta.gwl@gmail.com>
2018-02-08 13:11:30 +0530

    update perftest

--- benchmark/perfTest.js
+++ benchmark/perfTest.js
@@ -3,7 +3,6 @@ var suite = new Benchmark.Suite("XML Parser benchmark");
 
 var parser = require("../src/parser");
 var xml2js = require("xml2js");
-var parser2 = require("../src/parserV2");
 
 var fs = require("fs");
 var path = require("path");
@@ -11,27 +10,16 @@ var fileNamePath = path.join(__dirname, "../spec/assets/sample.xml");
 var xmlData = fs.readFileSync(fileNamePath).toString();
 
 suite
-.add('xml to json + validation V1', function() {
+.add('validate', function() {
   parser.validate(xmlData); 
+})
+.add('parse', function() {
   parser.parse(xmlData); 
 })
-.add('xml to json + validation V2 ', function() {
-  parser2.parse(xmlData);
-})
-.add('xml2js ', function() {
+/* .add('xml2js ', function() {
   xml2js.parseString(xmlData,function(err,result){
     if (err) throw err;
   });
-})
-/* .add('xml2js', {
-  'defer': true,
-  'fn' : function(deferred) {
-      xml2js.parseString(xmlData,function(err,result){
-        console.log("err", err);
-        if (err) throw err;
-        deferred.resolve();
-      });
-    }
 }) */
 
 .on('start',function(){
@@ -49,7 +37,7 @@ suite
 // add listeners 
 .on('complete', function() {
   for (var j = 0; j < this.length; j++) {
-    console.log(this[j].name + ":" +   this[j].hz + " requests/second");
+    console.log(this[j].name + " : " +   this[j].hz + " requests/second");
   }
 })
 // run async 
