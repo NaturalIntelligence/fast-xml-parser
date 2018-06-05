5fd816a28859a4e16c1e87f79d4dd4006bce140f
Amit Gupta <amitgupta.gwl@gmail.com>
2018-06-05 09:27:42 +0530

    fix test for demo app

--- spec/demoapp_spec.js
+++ spec/demoapp_spec.js
@@ -30,7 +30,7 @@ describe("DemoApp", function() {
 
         it("should parse xml to json", function(done) {
             //browser.assert.input('#textNodeConversion', true);
-            browser.pressButton("#submit");
+            browser.pressButton("#toJson");
             browser.assert.text("#result",
                                 "{ \"any_name\": { \"person\": [ { \"phone\": [ 122233344550, 122233344551 ], \"name\": \"Jack\", \"age\": 33, \"married\": \"Yes\", \"birthday\": \"Wed, 28 Mar 1979 12:13:14 +0300\", \"address\": [ { \"city\": \"New York\", \"street\": \"Park Ave\", \"buildingNo\": 1, \"flatNo\": 1 }, { \"city\": \"Boston\", \"street\": \"Centre St\", \"buildingNo\": 33, \"flatNo\": 24 } ] }, { \"phone\": [ 122233344553, 122233344554 ], \"name\": \"Boris\", \"age\": 34, \"married\": \"Yes\", \"birthday\": \"Mon, 31 Aug 1970 02:03:04 +0300\", \"address\": [ { \"city\": \"Moscow\", \"street\": \"Kahovka\", \"buildingNo\": 1, \"flatNo\": 2 }, { \"city\": \"Tula\", \"street\": \"Lenina\", \"buildingNo\": 3, \"flatNo\": 78 } ] } ] } }");
             done();
