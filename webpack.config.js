a0a0cca7148ddb922c2bc1d49b9f738d82c7d4a7
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2018-08-22 08:27:46 +0300

    fix(build): remove nodeJS bundle, update dependencies, remove unmaintained versions of NodeJS from travis test

--- webpack.config.js
+++ webpack.config.js
@@ -21,32 +21,5 @@ module.exports = [
             ]
         },
         target: "web"
-    },
-    {
-        context: __dirname,
-        entry: "./src/parser.js",
-        mode: "development",
-        devtool: "source-map",
-        output: {
-            path: __dirname,
-            filename: "./lib/parser.node.js",
-            devtoolModuleFilenameTemplate: "[resource-path]",
-            library: "parser",
-            libraryTarget: "commonjs2"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: "babel-loader"
-                }
-            ]
-        },
-        externals: Object.keys(require("./package.json").dependencies)
-                         .reduce(function(acc, key) {
-                             acc[key] = `commonjs ${key}`;
-                             return acc;
-                         }, {}),
-        target: "node"
     }
 ];
