10bba68e8e32350cfd0014f2db1bf351614253cd
Zeeshan Ahmad <ziishaned@gmail.com>
2019-01-15 05:12:44 +0400

    Add prettier and code cleanup (#136)
    * Add prettier as a dev dependency
    * Add .prettierrc
    * Add prettier command in scripts
    * Run prettier command
    * Formating

--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -1,17 +1,17 @@
-//Display message after instalation
+// Display message after instalation
 
 const stars = '\x1B[1m***\x1B[0m';
-const yellow = "\033[1;33m";
-//const light_green = "\033[1;32m";
-const light_blue = "\033[1;34m";
-const NC="\033[0m"; // No Color
+const yellow = '\033[1;33m';
+// const light_green = "\033[1;32m";
+const light_blue = '\033[1;34m';
+const NC = '\033[0m'; // No Color
 
 console.log('' + stars + ' Thank you for using fast-xml-parser! ' + stars);
 console.log('');
-console.log(yellow+"Donating to an open source project is more affordable"+NC);
+console.log(yellow + 'Donating to an open source project is more affordable' + NC);
 console.log(light_blue + '  https://opencollective.com/fast-xml-parser/donate' + NC);
-console.log( light_blue + '  https://www.patreon.com/bePatron?u=9531404' + NC);
-console.log( light_blue + '  https://www.paypal.me/amitkumarguptagwl' + NC);
+console.log(light_blue + '  https://www.patreon.com/bePatron?u=9531404' + NC);
+console.log(light_blue + '  https://www.paypal.me/amitkumarguptagwl' + NC);
 console.log('');
 console.log('');
 process.exit(0);
