f538c1d0fb7257da8e4f1454fb2853fa048200f4
Amit Gupta <amitgupta.gwl@gmail.com>
2019-03-06 07:40:48 +0530

    don't show postinstall msg when SUPPRESS_SUPPORT env is set

--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -1,17 +1,30 @@
-// Display message after instalation
+#!/usr/bin/env node
 
-const stars = '\x1B[1m***\x1B[0m';
-const yellow = '\033[1;33m';
-// const light_green = "\033[1;32m";
-const light_blue = '\033[1;34m';
-const NC = '\033[0m'; // No Color
+function main() {
+  if (process.env.SUPPRESS_SUPPORT) {
+    return;
+  }
 
-console.log('' + stars + ' Thank you for using fast-xml-parser! ' + stars);
-console.log('');
-console.log(yellow + 'Donating to an open source project is more affordable' + NC);
-console.log(light_blue + '  https://opencollective.com/fast-xml-parser/donate' + NC);
-console.log(light_blue + '  https://www.patreon.com/bePatron?u=9531404' + NC);
-console.log(light_blue + '  https://www.paypal.me/amitkumarguptagwl' + NC);
-console.log('');
-console.log('');
-process.exit(0);
+  const msg = '\u001b[32mLove fast-xml-parser? You can now support the project on following:\u001b[22m\u001b[39m\n > \u001b[96m\u001b[1mhttps://opencollective.com/fast-xml-parser/donate\u001b[0m\n > \u001b[96m\u001b[1mhttps://www.patreon.com/bePatron?u=9531404\u001b[0m\n';
+  try {
+    const Configstore = require('configstore');
+    const pkg = require(__dirname + '/../package.json');
+    const now = Date.now();
+
+    var week = 1000 * 60 * 60 * 24 * 7;
+
+    // create a Configstore instance with an unique ID e.g.
+    // Package name and optionally some default values
+    const conf = new Configstore(pkg.name);
+    const last = conf.get('lastCheck');
+    
+    if (!last || now - week > last) {
+      console.log(msg);
+      conf.set('lastCheck', now);
+    }
+  } catch (e) {
+    console.log(msg);
+  }
+}
+
+main();
