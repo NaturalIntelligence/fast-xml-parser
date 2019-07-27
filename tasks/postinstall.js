52d0ef971405c6f373b34d3c05a3180e928415f8
Amit Gupta <amitgupta.gwl@gmail.com>
2019-07-27 20:35:05 +0530

    remove configstore dependency

--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
 
-function main() {
-  if (process.env.SUPPRESS_SUPPORT) {
-    return;
-  }
 
-  const msg = '\u001b[32mLove fast-xml-parser? You can now support the project on following:\u001b[22m\u001b[39m\n > \u001b[96m\u001b[1mhttps://opencollective.com/fast-xml-parser/donate\u001b[0m\n > \u001b[96m\u001b[1mhttps://www.patreon.com/bePatron?u=9531404\u001b[0m\n';
-  try {
-    const Configstore = require('configstore');
-    const pkg = require(__dirname + '/../package.json');
-    const now = Date.now();
-
-    var week = 1000 * 60 * 60 * 24 * 7;
-
-    // create a Configstore instance with an unique ID e.g.
-    // Package name and optionally some default values
-    const conf = new Configstore(pkg.name);
-    const last = conf.get('lastCheck');
-    
-    if (!last || now - week > last) {
-      console.log(msg);
-      conf.set('lastCheck', now);
-    }
-  } catch (e) {
-    console.log(msg);
-  }
-}
-
-main();
+const msg = '\u001b[96m\u001b[1mLove fast-xml-parser? Check \u001b[32mhttps://amitkumargupta.work \u001b[96m\u001b[1mfor more projects and contribution.\u001b[0m\n';
+console.log(msg)
