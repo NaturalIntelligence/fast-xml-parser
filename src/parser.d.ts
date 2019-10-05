4a49683f64b4af51d41a63d2f16f243ba3200198
Sander Verweij <sverweij@users.noreply.github.com>
2019-10-05 04:22:27 +0200

    bugfix(types): add stopNodes to X2jOptions (#192)
    so they can be used cleanly in typescript.
    
    Fixes #191

--- src/parser.d.ts
+++ src/parser.d.ts
@@ -15,6 +15,7 @@ type X2jOptions = {
   parseTrueNumberOnly: boolean;
   tagValueProcessor: (tagValue: string, tagName: string) => string;
   attrValueProcessor: (attrValue: string, attrName: string) => string;
+  stopNodes: string[];
 };
 type X2jOptionsOptional = Partial<X2jOptions>;
 type validationOptions = {
