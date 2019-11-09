dbf2b51bcd528398e7cf4989a7775bf08b981fef
Amit Gupta <amitgupta.gwl@gmail.com>
2019-11-09 14:04:57 +0530

    [AG] fix #196 parsing of float ending with 0.

--- spec/xmlParser_spec.js
+++ spec/xmlParser_spec.js
@@ -34,7 +34,7 @@ describe("XMLParser", function() {
         <tag>value</tag>
         <boolean>true</boolean>
         <intTag>045</intTag>
-        <floatTag>65.34</floatTag>
+        <floatTag>65.340</floatTag>
         <long>420926189200190257681175017717</long>
         </rootNode>`;
         const expected = {
