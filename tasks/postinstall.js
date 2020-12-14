77d13ad3de9335711c588bc9290917020cd76e95
Gareth Jones <jones258@gmail.com>
2020-12-15 07:49:21 +1300

    fix: remove `postinstall` script

--- tasks/postinstall.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-
-const msg = '\u001b[96m\u001b[1mLove fast-xml-parser? Check \u001b[32mhttps://amitkumargupta.work \u001b[96m\u001b[1mfor more projects and contribution.\u001b[0m\n';
-console.log(msg)
