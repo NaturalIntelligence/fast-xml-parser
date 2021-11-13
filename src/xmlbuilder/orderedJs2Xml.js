54c7232afc2c457d1ec53212e48dc7616f03f486
Amit Gupta <amitgupta.gwl@gmail.com>
2021-11-13 14:53:35 +0530

    jsx: use indentation

--- src/xmlbuilder/orderedJs2Xml.js
+++ src/xmlbuilder/orderedJs2Xml.js
@@ -1,6 +1,7 @@
+const {EOL} = require('os');
 
 function toXml(jObj, options){
-    return arrToStr( [jObj], options);
+    return arrToStr( [jObj], options, 0);
 }
 
 function arrToStr(arr, options, level){
@@ -10,24 +11,28 @@ function arrToStr(arr, options, level){
         const tagName = propName(tagObj);
         const attStr = attr_to_str(tagObj.attributes, options);
         const propCount = Object.keys(tagObj[tagName]).length;
+        let leafNode = false;
 
-        let tagStart = `<${tagName}${attStr}`;
+        let indentation = "";
+        if(options.indentBy && options.indentBy.length > 0){
+            indentation = EOL + "" + options.indentBy.repeat(level);
+        }
+        let tagStart =  indentation + `<${tagName}${attStr}`;
         let tagValue = "";
         if( propCount === 0){
             //xmlStr += "";
         }else  if(propCount === 1 && tagObj[tagName][0][options.textNodeName]){
+            leafNode = true;
             tagValue = options.tagValueProcessor( tagObj[tagName][0][options.textNodeName] );
         }else  if(propCount === 1 && tagObj[tagName][0][options.cdataTagName]){
             tagValue = `<![CDATA[${tagObj[tagName][0][options.cdataTagName]}]]`;
         }else{
-            tagValue = arrToStr(tagObj[tagName], options, level + 1);;
+            tagValue = arrToStr(tagObj[tagName], options, level + 1);
         }
 
-        if(tagValue.length === 0 && options.suppressEmptyNode){
-            xmlStr += tagStart + "/>";
-        }else{
-            xmlStr += tagStart + `>${tagValue}</${tagName}>` ;
-        }
+        if(tagValue.length === 0 && options.suppressEmptyNode){ xmlStr += tagStart + "/>"; }
+        else if(leafNode) { xmlStr += tagStart + `>${tagValue}</${tagName}>`; }
+        else { xmlStr += tagStart + `>${tagValue}${indentation}</${tagName}>` ;}
     }
     return xmlStr;
 }
