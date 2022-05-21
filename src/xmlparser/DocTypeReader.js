50452b24d339473c2cb8547a52efdeefbc101af0
Hati- <hati.goshujin@gmail.com>
2022-05-21 05:41:00 +0200

    fix indent misalignment

--- src/xmlparser/DocTypeReader.js
+++ src/xmlparser/DocTypeReader.js
@@ -3,11 +3,11 @@ function readDocType(xmlData, i){
     
     const entities = {};
     if( xmlData[i + 3] === 'O' &&
-         xmlData[i + 4] === 'C' &&
-         xmlData[i + 5] === 'T' &&
-         xmlData[i + 6] === 'Y' &&
-         xmlData[i + 7] === 'P' &&
-         xmlData[i + 8] === 'E')
+        xmlData[i + 4] === 'C' &&
+        xmlData[i + 5] === 'T' &&
+        xmlData[i + 6] === 'Y' &&
+        xmlData[i + 7] === 'P' &&
+        xmlData[i + 8] === 'E')
     {    
         i = i+9;
         let angleBracketsCount = 1;
@@ -16,50 +16,50 @@ function readDocType(xmlData, i){
         for(;i<xmlData.length;i++){
             if (xmlData[i] === '<') {
                 if( hasBody && 
-                     xmlData[i+1] === '!' &&
-                     xmlData[i+2] === 'E' &&
-                     xmlData[i+3] === 'N' &&
-                     xmlData[i+4] === 'T' &&
-                     xmlData[i+5] === 'I' &&
-                     xmlData[i+6] === 'T' &&
-                     xmlData[i+7] === 'Y'
+                    xmlData[i+1] === '!' &&
+                    xmlData[i+2] === 'E' &&
+                    xmlData[i+3] === 'N' &&
+                    xmlData[i+4] === 'T' &&
+                    xmlData[i+5] === 'I' &&
+                    xmlData[i+6] === 'T' &&
+                    xmlData[i+7] === 'Y'
                 ){
                     i += 7;
                     entity = true;
                 }else if( hasBody && 
                     xmlData[i+1] === '!' &&
-                     xmlData[i+2] === 'E' &&
-                     xmlData[i+3] === 'L' &&
-                     xmlData[i+4] === 'E' &&
-                     xmlData[i+5] === 'M' &&
-                     xmlData[i+6] === 'E' &&
-                     xmlData[i+7] === 'N' &&
-                     xmlData[i+8] === 'T'
+                    xmlData[i+2] === 'E' &&
+                    xmlData[i+3] === 'L' &&
+                    xmlData[i+4] === 'E' &&
+                    xmlData[i+5] === 'M' &&
+                    xmlData[i+6] === 'E' &&
+                    xmlData[i+7] === 'N' &&
+                    xmlData[i+8] === 'T'
                 ){
                     //Not supported
                     i += 8;
                 }else if( hasBody && 
                     xmlData[i+1] === '!' &&
-                     xmlData[i+2] === 'A' &&
-                     xmlData[i+3] === 'T' &&
-                     xmlData[i+4] === 'T' &&
-                     xmlData[i+5] === 'L' &&
-                     xmlData[i+6] === 'I' &&
-                     xmlData[i+7] === 'S' &&
-                     xmlData[i+8] === 'T'
+                    xmlData[i+2] === 'A' &&
+                    xmlData[i+3] === 'T' &&
+                    xmlData[i+4] === 'T' &&
+                    xmlData[i+5] === 'L' &&
+                    xmlData[i+6] === 'I' &&
+                    xmlData[i+7] === 'S' &&
+                    xmlData[i+8] === 'T'
                 ){
                     //Not supported
                     i += 8;
                 }else if( hasBody && 
                     xmlData[i+1] === '!' &&
-                     xmlData[i+2] === 'N' &&
-                     xmlData[i+3] === 'O' &&
-                     xmlData[i+4] === 'T' &&
-                     xmlData[i+5] === 'A' &&
-                     xmlData[i+6] === 'T' &&
-                     xmlData[i+7] === 'I' &&
-                     xmlData[i+8] === 'O' &&
-                     xmlData[i+9] === 'N'
+                    xmlData[i+2] === 'N' &&
+                    xmlData[i+3] === 'O' &&
+                    xmlData[i+4] === 'T' &&
+                    xmlData[i+5] === 'A' &&
+                    xmlData[i+6] === 'T' &&
+                    xmlData[i+7] === 'I' &&
+                    xmlData[i+8] === 'O' &&
+                    xmlData[i+9] === 'N'
                 ){
                     //Not supported
                     i += 9;
