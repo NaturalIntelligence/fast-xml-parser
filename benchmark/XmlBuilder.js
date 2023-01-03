a0a7b6c404e7ecc84f1c7e6d5928e66b41eaa08a
MDe <MDe@knowis.de>
2023-01-03 13:29:10 +0100

    Revert formatting changes

--- benchmark/XmlBuilder.js
+++ benchmark/XmlBuilder.js
@@ -3,7 +3,7 @@
 const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite("XML Builder benchmark");
 
-const { XMLBuilder } = require("../src/fxp");
+const {XMLBuilder} = require("../src/fxp");
 const xml2js = require("xml2js");
 const xml2jsBuilder = new xml2js.Builder();
 
@@ -15,36 +15,36 @@ const jsonData = JSON.parse(fs.readFileSync(fileNamePath).toString());
 const jsonOrderedData = JSON.parse(fs.readFileSync(fileNamePath2).toString());
 
 const fxpBuilder = new XMLBuilder();
-const fxpBuilderForOrderedJs = new XMLBuilder({ preserveOrder: true });
+const fxpBuilderForOrderedJs = new XMLBuilder({preserveOrder: true});
 
 suite
-    .add("fxp", function () {
+    .add("fxp", function() {
         fxpBuilder.build(jsonData);
     })
-    .add("fxp - preserve order", function () {
+    .add("fxp - preserve order", function() {
         fxpBuilderForOrderedJs.build(jsonOrderedData);
     })
-    .add('xml2js ', function () {
+    .add('xml2js ', function() {
         xml2jsBuilder.buildObject(jsonData);
     })
 
-    .on("start", function () {
+    .on("start", function() {
         console.log("Running Suite: " + this.name);
     })
-    .on("error", function (e) {
+    .on("error", function(e) {
         console.log("Error in Suite: " + this.name, e);
     })
-    .on("abort", function (e) {
+    .on("abort", function(e) {
         console.log("Aborting Suite: " + this.name, e);
     })
     /*.on('cycle',function(event){
         console.log("Suite ID:" + event.target.id);
     })*/
     // add listeners
-    .on("complete", function () {
+    .on("complete", function() {
         for (let j = 0; j < this.length; j++) {
             console.log(this[j].name + " : " + this[j].hz + " requests/second");
         }
     })
     // run async
-    .run({ "async": true });
+    .run({"async": true});
--- benchmark/XmlParser.js
+++ benchmark/XmlParser.js
@@ -3,7 +3,7 @@
 const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite("XML Parser benchmark");
 
-const { XMLParser } = require("../src/fxp");
+const {XMLParser} = require("../src/fxp");
 const xml2js = require("xml2js");
 const fxpv3 = require("fast-xml-parser");
 const { convert } = require('xmlbuilder2');
@@ -18,49 +18,49 @@ const fileNamePath = path.join(__dirname, "../spec/assets/ptest.xml");//with CDA
 const xmlData = fs.readFileSync(fileNamePath).toString();
 
 const fxpParser = new XMLParser();
-const fxpParserForOrderedJs = new XMLParser({ preserveOrder: true });
+const fxpParserForOrderedJs = new XMLParser({preserveOrder: true});
 
 suite
-    .add("fxp v3", function () {
+    .add("fxp v3", function() {
         fxpv3.parse(xmlData);
     })
-    .add("fxp", function () {
+    .add("fxp", function() {
         fxpParser.parse(xmlData);
     })
-    .add("fxp - preserve order", function () {
+    .add("fxp - preserve order", function() {
         fxpParserForOrderedJs.parse(xmlData);
     })
-    .add("xmlbuilder2", function () {
+    .add("xmlbuilder2", function() {
         convert(xmlData);
     })
-    .add('xml2js ', function () {
-        xml2js.parseString(xmlData, function (err, result) {
-            if (err) throw err;
-            // 2 for sample, 18432 for midsize, 138240 for large
-            //if(2 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
-            //if(18432 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
-            //if(2 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
-            // if(138240 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
-        });
+    .add('xml2js ', function() {
+      xml2js.parseString(xmlData,function(err,result){
+        if (err) throw err;
+        // 2 for sample, 18432 for midsize, 138240 for large
+        //if(2 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
+        //if(18432 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
+        //if(2 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
+        // if(138240 !== result["any_name"]["person"].length) console.log("incorrect length", result["any_name"]["person"].length);
+      });
     })
 
-    .on("start", function () {
+    .on("start", function() {
         console.log("Running Suite: " + this.name);
     })
-    .on("error", function (e) {
+    .on("error", function(e) {
         console.log("Error in Suite: " + this.name, e);
     })
-    .on("abort", function (e) {
+    .on("abort", function(e) {
         console.log("Aborting Suite: " + this.name, e);
     })
     /*.on('cycle',function(event){
         console.log("Suite ID:" + event.target.id);
     })*/
     // add listeners
-    .on("complete", function () {
+    .on("complete", function() {
         for (let j = 0; j < this.length; j++) {
             console.log(this[j].name + " : " + this[j].hz + " requests/second");
         }
     })
     // run async
-    .run({ "async": true });
+    .run({"async": true});
