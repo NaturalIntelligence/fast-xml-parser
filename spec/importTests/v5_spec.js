42712223112bcdf3b198b4573ca86489cd4d2c5c
amit kumar gupta <amitgupta.gwl@gmail.com>
2024-09-21 08:54:39 +0530

    fix syntax in docs to import v5

---
+++ spec/importTests/v5_spec.js
@@ -0,0 +1,8 @@
+const XMLParser = require("fast-xml-parser/src/v5/XMLParser");
+const XMLParser = require("fast-xml-parser/src/v5/XMLParser");
+
+describe("XMLParser", function() {
+  it("should import v5", function() {
+    const parser = new XMLParser();
+  });
+});
