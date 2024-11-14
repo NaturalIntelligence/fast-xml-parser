eb8aa89a3cdf195164d120c60d4bf0d6e730488f
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-14 14:28:40 +0200

    use XML instead of XMl (#695)

--- spec/html_spec.js
+++ spec/html_spec.js
@@ -16,7 +16,7 @@ describe("XMLParser", function() {
                     gtag('config', 'UA-80202630-2');
                 </script>
         
-                <title>Fast XMl Parser</title>
+                <title>Fast XML Parser</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" href="static/css/bootstrap.min.css">
